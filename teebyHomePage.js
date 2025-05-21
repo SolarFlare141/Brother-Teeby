cde1b1afe4aaeea119bc45e3ca55fad0342de2f7
Solar141 <14jamiesawyer@gmail.com>
2025-05-21 19:11:55 +0100

    Add files via upload

---
+++ teebyHomePage.js
@@ -0,0 +1,12 @@
+function lightMode(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+}
+function darkMode(){
+    document.body.style.backgroundColor="black";
+    document.body.style.color="white";
+}
+function Reset(){
+    document.body.style.backgroundColor="DarkOliveGreen";
+    document.body.style.color="white";
+}
